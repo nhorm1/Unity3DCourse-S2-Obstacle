2614fa0ce0695e1321d83e061c6808b657b39a69
Jay <nhorm1@github.com>
2023-01-17 00:26:44 +0700

    1. Make dropper display time dropped once. 2. Allow reset hit after time passed. 3. Add spinner.

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
   MeshRenderer dropperRenderer;
   Rigidbody dropperRigidbody;
+  bool dropped = false;
   [SerializeField] float timeToWait = 3f;
 
   // Start is called before the first frame update
@@ -21,10 +23,11 @@ public class Dropper : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    if(Time.time > timeToWait)
+    if (!dropped && Time.time > timeToWait)
     {
       dropperRenderer.enabled = true;
       dropperRigidbody.useGravity = true;
+      dropped = true;
 
       Debug.Log(timeToWait + " seconds has elapsed");
     }
--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+  [SerializeField] float resetHitTime = 3f;
+  float lastHitTime = 0f;
+  Color origColor;
+
+  private void Start()
+  {
+    origColor = GetComponent<MeshRenderer>().material.color;
+  }
+
+  private void Update()
+  {
+    if( resetHitTime > 0 && 
+        lastHitTime > 0 &&
+        ( Time.time > (lastHitTime + resetHitTime) ) )
+    {
+      GetComponent<MeshRenderer>().material.color = origColor;
+      gameObject.tag = "Untagged";
+    }
+  }
+
   private void OnCollisionEnter(Collision collision)
   {
     if(collision.gameObject.tag == "Player")
     {
+      if (gameObject.tag != "Hit")
+      {
+        lastHitTime = Time.time;
+      }
+
       GetComponent<MeshRenderer>().material.color = Color.magenta;
       gameObject.tag = "Hit";
     }
